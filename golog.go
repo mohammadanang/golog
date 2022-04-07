a99cbc92c11042316546cba85fce224f833fc19d
mohammadanang <m.anangnur@gmail.com>
2022-04-07 21:34:49 +0700

    update function dependency

--- golog.go
+++ golog.go
@@ -4,4 +4,8 @@ import "github.com/gookit/color"
 
 func Success(message string) {
 	color.Success.Print(message, "\n")
+
+	color.Success.Prompt(message, "\n")
+
+	color.Success.Tips(message, "\n")
 }
